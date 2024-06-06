001fa9097a658c9b521daabcd555b9242a09ce72
Jay Vegad <jayvegad10@gmail.com>
2024-06-06 20:01:46 +0530

    Making card best

--- componants/NewsItems.jsx
+++ componants/NewsItems.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
-const NewsItems = ({ title, description, url, url1 }) => {
+const NewsItems = ({ title, description, url, url1, mode1, textcolor }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
+  const descriptionHeight = showMore ? 'auto' : '170px';
+  const showReadMoreButton = description.length > descriptionHeight.replace('px', '');
+
   return (
     <>
-      <div className="card mx-auto" style={{ width: '18rem', height: '30rem' }}>
-        <img
-          src={url}
-          className="card-img-top"
-          alt="..."
-          style={{
-            height: '200px',
-            width: '100%',
-            objectFit: 'cover',
-          }}
-        />
-        <div className="card-body d-flex flex-column justify-content-between">
+      <div className="card mx-auto" style={{ width: '18rem', height: '35rem' }}>
+        <div style={{ height: '200px', width: '100%', overflow: 'hidden' }}>
+          <img
+            src={url}
+            className="card-img-top"
+            alt="..."
+            style={{
+              height: '100%',
+              width: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <div className={`card-body d-flex flex-column justify-content-between bg-${mode1} text-${textcolor}`}>
           <div>
             <h5 className="card-title">{title}</h5>
-            <p className="card-text" style={{ height: '100px', overflow: 'auto' }}>
+            <p className="card-text" style={{ height: descriptionHeight, overflow: 'hidden' }}>
               {description}
             </p>
           </div>
-          <a href={url1} className="btn btn-primary align-self-end" target="_blank" rel="noreferrer">
-            Read More
-          </a>
+          <div className='d-flex justify-content-between'>
+            {showReadMoreButton ? (
+              showMore ? (
+                <button className="btn btn-primary align-self-end" onClick={toggleShowMore}>
+                  Show Less
+                </button>
+              ) : (
+                <button className="btn btn-primary align-self-end" onClick={toggleShowMore}>
+                  Read More
+                </button>
+              )
+            ) : null}
+            <a href={url1} className="btn btn-primary align-self-end" target="_blank" rel="noreferrer">
+              Visit
+            </a>
+          </div>
         </div>
       </div>
     </>
