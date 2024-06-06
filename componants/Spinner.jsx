5ccbfc1e554a64d0372fdee7371773eca3c0e31b
Jay Vegad <jayvegad10@gmail.com>
2024-06-06 20:02:24 +0530

    Spinner is dont

--- componants/Spinner.jsx
+++ componants/Spinner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import img from './Loding.gif'
+import img from './Loading2.gif'
 export const Spinner = () => {
   return (
     <div className='text-center'>
-        <img src={img} alt="" />
+        <img src={img} />
     </div>
   )
 }
