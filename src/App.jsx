eb9a395f2778b56b58012fc200dd5d163c302d26
Jay Vegad <jayvegad10@gmail.com>
2024-06-06 20:03:21 +0530

    Changes in compo

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import React, { useState } from 'react';
 import Navbar from '../componants/Navbar.jsx';
 import News from '../componants/News.jsx';
 const App = () => {
-  const [mode, useMode] = useState('light')
+  const [mode, useMode] = useState("light")
+  const [modemsg, setmsgMode] = useState("Enable Dark Mode");
+  const [color, setTextcolor] = useState("black")
   function toggleMode() {
-    if(mode == 'light') {
-      useMode('dark');
+    if(mode === "light") {
+      useMode("dark");
+      document.body.style.backgroundColor='#212529';
+      setmsgMode("Enable Light Mode");
+      setTextcolor("white");
     }
     else {
+      setmsgMode("Enable Dark Mode");
+      document.body.style.backgroundColor='white';
       useMode('light');
+      setTextcolor("black");
     }
    }
   return (
     <>
-      <Navbar mode={mode} togglemode={toggleMode} />
-      <News />
+      <Navbar mode={mode} togglemode={toggleMode} togglemsg={modemsg} textcolor={color}/>
+      <News mode={mode} togglemode={toggleMode} textcolor={color}/>
     </>
   );
 };
