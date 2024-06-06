154e051ca922ae1509eed057189c4ddc068e16e0
Jay Vegad <jayvegad10@gmail.com>
2024-06-06 20:00:24 +0530

    Night mode is now complete

--- componants/Navbar.jsx
+++ componants/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
-const Navbar = ({ mode, togglemode }) => {
+import './index.css'
+const Navbar = (props) => {
   return (
     <>
-      <nav className={`navbar navbar-expand-lg bg-body-tertiary justify-content-between bg-${mode}`}>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} text-${props.textcolor} fixed-top`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
+          <a className="navbar-brand" href="https://github.com/Vegadjay">
+            Click Here
           </a>
           <button
             className="navbar-toggler"
@@ -22,29 +22,19 @@ const Navbar = ({ mode, togglemode }) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
                 <div className="form-check form-switch">
                   <input
                     className="form-check-input"
                     type="checkbox"
                     role="switch"
                     id="flexSwitchCheckChecked"
-                    onClick={togglemode}
+                    onClick={props.togglemode}
                   />
                   <label
                     className="form-check-label"
                     htmlFor="flexSwitchCheckChecked"
                   >
-                    Enable Dark Mode
+                    <span>{props.togglemsg}</span>
                   </label>
                 </div>
               </li>
