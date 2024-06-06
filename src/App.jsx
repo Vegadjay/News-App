78196f017318f463945d39b1bb5b68db367d4436
Jay Vegad <jayvegad10@gmail.com>
2024-06-06 13:15:02 +0530

    Adding dark mode function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import Navbar from '../componants/Navbar.jsx'
-import News from '../componants/News.jsx'
+import React, { useState } from 'react';
+import Navbar from '../componants/Navbar.jsx';
+import News from '../componants/News.jsx';
 const App = () => {
+  const [mode, useMode] = useState('light')
+  function toggleMode() {
+    if(mode == 'light') {
+      useMode('dark');
+    }
+    else {
+      useMode('light');
+    }
+   }
   return (
     <>
-      <Navbar />
+      <Navbar mode={mode} togglemode={toggleMode} />
       <News />
     </>
   );
