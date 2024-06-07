76735102290442cd619765f5207b0baf05358134
Jay Vegad <jayvegad10@gmail.com>
2024-06-07 20:23:31 +0530

    Update Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from '../componants/Navbar.jsx';
 import News from '../componants/News.jsx';
+
 const App = () => {
-  const [mode, useMode] = useState("light")
-  const [modemsg, setmsgMode] = useState("Enable Dark Mode");
-  const [color, setTextcolor] = useState("black")
+  const [mode, setMode] = useState("light");
+  const [modemsg, setModemsg] = useState("Enable Dark Mode");
+  const [color, setTextcolor] = useState("black");
+
   function toggleMode() {
-    if(mode === "light") {
-      useMode("dark");
-      document.body.style.backgroundColor='#212529';
-      setmsgMode("Enable Light Mode");
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = '#212529';
+      setModemsg("Enable Light Mode");
       setTextcolor("white");
-    }
-    else {
-      setmsgMode("Enable Dark Mode");
-      document.body.style.backgroundColor='white';
-      useMode('light');
+    } else {
+      setModemsg("Enable Dark Mode");
+      document.body.style.backgroundColor = 'white';
+      setMode('light');
       setTextcolor("black");
     }
-   }
+  }
+
   return (
     <>
-      <Navbar mode={mode} togglemode={toggleMode} togglemsg={modemsg} textcolor={color}/>
-      <News mode={mode} togglemode={toggleMode} textcolor={color}/>
+      <BrowserRouter>
+        <Navbar mode={mode} togglemode={toggleMode} togglemsg={modemsg} textcolor={color} />
+        <Routes>
+          <Route path="/sports" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="sports" />} />
+          <Route path="/business" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="business" />} />
+          <Route path="/entertainment" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="entertainment" />} />
+          <Route path="/general" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="general" />} />
+          <Route path="/health" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="health" />} />
+          <Route path="/science" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="science" />} />
+          <Route path="/sports" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="sports" />} />
+          <Route path="/technology" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="technology" />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
