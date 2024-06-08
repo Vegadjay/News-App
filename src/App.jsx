6d35abb6dc7e2529acda4e4fa76454c760259b1f
Jay Vegad <jayvegad10@gmail.com>
2024-06-08 11:12:20 +0530

    Adding Router And Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 import Navbar from '../componants/Navbar.jsx';
 import News from '../componants/News.jsx';
 
@@ -7,6 +8,7 @@ const App = ({ category = 'general' }) => {
   const [mode, setMode] = useState("light");
   const [modemsg, setModemsg] = useState("Enable Dark Mode");
   const [color, setTextcolor] = useState("black");
+  const [progress, setProgress] = useState(0);
 
   function toggleMode() {
     if (mode === "light") {
@@ -26,15 +28,23 @@ const App = ({ category = 'general' }) => {
     <>
       <BrowserRouter>
         <Navbar mode={mode} togglemode={toggleMode} togglemsg={modemsg} textcolor={color} />
+        <LoadingBar
+          color='#f11946'
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+          height={1}
+          className="position-fixed top-0 start-0 end-0"
+          style={{height: '5px'}}
+        />
         <Routes>
-          <Route path="/" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category={category} />} />
-          <Route path="/sports" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="sports" />} />
-          <Route path="/business" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="business" />} />
-          <Route path="/entertainment" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="entertainment" />} />
-          <Route path="/general" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="general" />} />
-          <Route path="/health" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="health" />} />
-          <Route path="/science" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="science" />} />
-          <Route path="/technology" element={<News mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="technology" />} />
+          <Route path="/" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category={category} />} />
+          <Route path="/sports" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="sports" />} />
+          <Route path="/business" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="business" />} />
+          <Route path="/entertainment" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="entertainment" />} />
+          <Route path="/general" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="general" />} />
+          <Route path="/health" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="health" />} />
+          <Route path="/science" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="science" />} />
+          <Route path="/technology" element={<News setProgress={setProgress} mode={mode} togglemode={toggleMode} textcolor={color} country="in" category="technology" />} />
         </Routes>
       </BrowserRouter>
     </>
