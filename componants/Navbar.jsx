84e46d14871fde8c793fc39d33ae2e85036776bf
Jay Vegad <jayvegad10@gmail.com>
2024-06-09 18:35:27 +0530

    Fixing Navbar

--- componants/Navbar.jsx
+++ componants/Navbar.jsx
@@ -6,112 +6,55 @@ const Navbar = (props) => {
   const location = useLocation();
 
   return (
-    <>
-      <nav
-        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} text-${props.textcolor} fixed-top`}
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="https://github.com/Vegadjay">
-            Check My Github
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
-              <li className="nav-item">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} fixed-top`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="https://github.com/Vegadjay">
+          Check My Github
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            {['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'].map((category) => (
+              <li className="nav-item" key={category}>
                 <a
-                  className={`nav-link ${location.pathname === "/general" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/general"
+                  className={`nav-link ${location.pathname === `/${category}` ? "active" : ""}`}
+                  href={`/${category}`}
+                  style={{ fontSize: '1rem' }}
                 >
-                  General
+                  {category.charAt(0).toUpperCase() + category.slice(1)}
                 </a>
               </li>
-              <li className="nav-item">
-                <a
-                  className={`nav-link ${location.pathname === "/business" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/business"
+            ))}
+            <li className="nav-item">
+              <div className="form-check form-switch">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="flexSwitchCheckChecked"
+                  onClick={props.togglemode}
+                />
+                <label
+                  className="form-check-label"
+                  htmlFor="flexSwitchCheckChecked"
                 >
-                  Business
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={`nav-link ${location.pathname === "/entertainment" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/entertainment"
-                >
-                  Entertainment
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={`nav-link ${location.pathname === "/health" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/health"
-                >
-                  Health
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={`nav-link ${location.pathname === "/science" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/science"
-                >
-                  Science
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={`nav-link ${location.pathname === "/sports" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/sports"
-                >
-                  Sports
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className={`nav-link ${location.pathname === "/technology" ? "active" : ""}`}
-                  aria-current="page"
-                  href="/technology"
-                >
-                  Technology
-                </a>
-              </li>
-              <li className="nav-item">
-                <div className="form-check form-switch">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    id="flexSwitchCheckChecked"
-                    onClick={props.togglemode}
-                  />
-                  <label
-                    className="form-check-label"
-                    htmlFor="flexSwitchCheckChecked"
-                  >
-                    <span>{props.togglemsg}</span>
-                  </label>
-                </div>
-              </li>
-            </ul>
-          </div>
+                  {props.togglemsg}
+                </label>
+              </div>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
