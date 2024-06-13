cc1aa3c7103bc638eae62d533f29f50ff7236e07
Jay Vegad <jayvegad10@gmail.com>
2024-06-13 16:54:55 +0530

    Changing in photo height and width

--- componants/NewsItems.jsx
+++ componants/NewsItems.jsx
@@ -1,5 +1,3 @@
-// NewsItems.jsx
-import React, { useState } from "react";
 import "./index.css";
 
 const NewsItems = ({
@@ -10,46 +8,42 @@ const NewsItems = ({
   mode1,
   textcolor,
   date,
+  newsfrom,
+  bordercolor
 }) => {
-  const [showMore, setShowMore] = useState(false);
-  const descriptionHeight = showMore ? "auto" : "200px";
 
   return (
     <div
-      className={`card bg-${mode1} text-${textcolor} h-100`}
-      style={{ width: "100%" }}
-    >
-      <img src={url} className="card-img-top" alt="..." />
-      <div className="card-body d-flex flex-column justify-content-between">
+      className={`card bg-${mode1} text-${textcolor} border border-${bordercolor}`}  style={{ minHeight: "550px" }}>
+      <img src={url} className="card-img-top" alt="Image not available" style={{ height: "200px", objectFit: "cover" }}/>
+      <div className="card-body">
         <div>
           <h5 className="card-title">{title}</h5>
+          <span>
+            <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-primary z-3">
+              This news is from : {newsfrom}
+            </span>
+          </span>
           <p
             className="card-text"
-            style={{ height: descriptionHeight, overflow: "hidden" }}
+            style={{ display: "-webkit-box", WebkitLineClamp: "3", WebkitBoxOrient: "vertical", overflow: "hidden" }}
           >
             {description}
           </p>
-          {description.length > 200 && (
-            <button
-              className={`btn btn-link text-${textcolor}`}
-              onClick={() => setShowMore(!showMore)}
-            >
-              {" "}
-              {showMore ? "Show Less" : "Show More"}{" "}
-            </button>
-          )}{" "}
         </div>{" "}
-        <p>Publish At: {date}</p>{" "}
-        <div className="d-flex justify-content-between align-items-center">
+        <div className="bg-dark">
+          <p className="m-3 text-danger fw-bold position-absolute top-25 start-0 mb-3 z-2">Publish At: {date}</p>{" "}
+        </div>
+        <div className="d-flex align-items-center position-absolute bottom-0 start-0 mt-2">
           {" "}
           <a
             href={url1}
-            className="btn btn-primary"
+            className="btn btn-primary m-5 mb-3"
             target="_blank"
             rel="noreferrer"
           >
             {" "}
-            Visit{" "}
+            Visit
           </a>{" "}
         </div>{" "}
       </div>{" "}
